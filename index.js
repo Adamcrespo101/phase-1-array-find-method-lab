e8cc8e541d3b6abbc50d4348713b40beaaa4830a
Adam <adam.crespo101@outlook.com>
2022-02-28 09:12:10 -0500

    commit tree 7797c35c9f80690170555eec4918e139efd87c77

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// code your solution here
+//code your solution here
+// function superbowlWin (record){
+// const superbowl = record.find(element => element.result === 'W')
+//       // if(superbowl) {
+//       //    return superbowl.year;
+//       // }  else {
+//       //    return undefined
+//       // }
+//       return superbowl ? superbowl.year : undefined
+//    }
+
+function superbowlWin (record) {
+let result = record.find(element => element.result === 'W')
+      return result ? result.year : undefined;
+}
